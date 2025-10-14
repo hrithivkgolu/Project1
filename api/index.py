2a5820dcbfcf3a5755f2ee8b35ffbccf46d440b0
hrithivk <hrithivkgo1111@gmail.com>
2025-10-14 21:36:30 +0530

    vercel run

--- api/index.py
+++ api/index.py
@@ -1,9 +1,1 @@
 from main import app
-from fastapi.responses import PlainTextResponse
-
-# For vercel-python serverless runtime
-def handler(request, context):
-    return PlainTextResponse("Use /api for FastAPI routes")
-
-# Export FastAPI app
-app = app
